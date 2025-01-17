1f814fab8ce95316344cafec238a83ce647b40c7
NestorDilanDuran <dd@mexicobusiness.mx>
2025-01-17 17:14:45 -0600

    mobile version

--- themes/custom/sph/js/scripts.js
+++ themes/custom/sph/js/scripts.js
@@ -1,11 +1,11 @@
-;(function ($) {
-  $(document).ready(function() {
+(function ($) {
+  $(document).ready(function () {
 
-    //Add link in logo
+
     $("#block-sph-svgimagelogo img").wrap('<a href="/"></a>');
 
-    //Scroll Down
-    $('a[href^="#"]').on('click', function(e) {
+   
+    $('a[href^="#"]').on('click', function (e) {
       e.preventDefault();
       var target = $(this.getAttribute('href'));
       if (target.length) {
@@ -16,4 +16,30 @@
     });
 
   });
-})(jQuery)
+})(jQuery);
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.getElementById('hamburger-menu');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const navegacionPrincipal = document.getElementById('block-sph-navegacionprincipal');
+  const languageSwitcher = document.getElementById('block-sph-languageswitcher');
+  
+  hamburger.addEventListener('click', function() {
+    this.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
+    
+    if (mobileMenu.classList.contains('open')) {
+    
+      setTimeout(() => {
+        navegacionPrincipal.classList.add('visible');
+      }, 100);
+      
+
+      setTimeout(() => {
+        languageSwitcher.classList.add('visible');
+      }, 200);
+    } else {
+      navegacionPrincipal.classList.remove('visible');
+      languageSwitcher.classList.remove('visible');
+    }
+  });
+});
